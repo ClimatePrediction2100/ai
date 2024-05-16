3a9349f6ec23320f2783b796fd4f9d9c1f148559
Jinho Heo <uj07077@gmail.com>
2024-05-16 15:21:28 +0900

    Feat: Add earlystop

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -3,25 +3,26 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from matplotlib import pyplot as plt
+import tqdm
 
 import os
 import config
 from config import DEVICE as device
 
-
-def train_model(model, train_loader, test_loader, num_epochs, criterion=nn.MSELoss(), optimizer=optim.Adam, learning_rate=0.001):
+def train_model(model, train_loader, test_loader, num_epochs, patience=10, criterion=nn.MSELoss(), optimizer=optim.Adam, learning_rate=0.001):
     # Train the model
     total_step = len(train_loader)
     
     optimizer = optimizer(model.parameters(), lr=learning_rate)
-    criterion = criterion
     model.to(device)
     
     min_loss = float('inf')
     model_path = os.path.join(config.ROOT_DIR, 'models', f'{model}')
-    os.mkdir(model_path, exist_ok=True)
-        
-    for epoch in range(num_epochs):
+    os.makedirs(model_path, exist_ok=True)  # Corrected from os.mkdir to os.makedirs
+    
+    epochs_no_improve = 0  # Counter to keep track of non-improving epochs
+    
+    for epoch in tqdm(range(num_epochs)):
         model.train()
         train_loss = 0
         for inputs, labels in train_loader:
@@ -33,7 +34,7 @@ def train_model(model, train_loader, test_loader, num_epochs, criterion=nn.MSELo
             optimizer.step()
             train_loss += loss.item()
         
-        train_loss /= len(train_loader)
+        train_loss /= total_step
         
         # Validation phase
         model.eval()
@@ -49,8 +50,19 @@ def train_model(model, train_loader, test_loader, num_epochs, criterion=nn.MSELo
         
         print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {test_loss}')
         
+        # Check if the loss improved
         if test_loss < min_loss:
             min_loss = test_loss
             torch.save(model.state_dict(), os.path.join(model_path, 'best_model.pth'))
             model_name = f'{model}_epoch_{epoch+1}'
             print(f"Saved improved {model_name} with Test Loss: {test_loss}")
+            epochs_no_improve = 0  # Reset the counter
+        else:
+            epochs_no_improve += 1  # Increment the counter
+        
+        # Early stopping check
+        if epochs_no_improve >= patience:
+            print(f"Stopping training early - no improvement in {patience} epochs.")
+            break
+
+    return model
