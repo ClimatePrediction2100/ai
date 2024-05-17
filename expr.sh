e68abc27807271655663bc9da6402c62373b304d
Jinho Heo <uj07077@gmail.com>
2024-05-17 15:02:05 +0900

    Fix: mismatch filename

--- expr.sh
+++ expr.sh
@@ -5,7 +5,7 @@ models=("lstm" "rnn" "gru" "mlp" "attn")
 num_layers=(2 4)
 hidden_dims=(100 200)
 loss_functions=("mse" "mae" "huber")
-batch_sizes=(64)  # Add your specific batch size values
+batch_sizes=(12)  # Add your specific batch size values
 epochs=(40)
 patiences=(10)
 learning_rates=(0.01 0.001 0.0001)
@@ -37,7 +37,7 @@ for model in "${models[@]}"; do
                                 for patience in "${patiences[@]}"; do
                                     ((counter++))
                                     echo "Running trial $counter/$total_experiments with model=$model, num_layers=$num_layer, hidden_dim=$hidden_dim, loss=$loss, batch_size=$batch_size, epoch=$epoch, lr=$lr, seq_length=$seq_length, patience=$patience"
-                                    python your_script.py "train" --model $model --num_layers $num_layer --hidden_dim $hidden_dim --loss $loss --batch_size $batch_size --epoch $epoch --lr $lr --seq_length $seq_length --patience $patience --save_model
+                                    python main.py "expr" --model $model --num_layers $num_layer --hidden_dim $hidden_dim --loss $loss --batch_size $batch_size --epoch $epoch --lr $lr --seq_length $seq_length --patience $patience --save_model
                                 done
                             done
                         done
