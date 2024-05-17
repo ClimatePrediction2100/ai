2ef6f6bd5c67de99d1e1e8842b6e04bec7e8c064
Jinho Heo <uj07077@gmail.com>
2024-05-18 08:38:15 +0900

    Refactor: mistypo

--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -5,22 +5,34 @@ from tqdm import tqdm
 import os
 import config
 
-def train_model(model, train_loader, test_loader, num_epochs, device, save_model=False, patience=10, criterion=nn.MSELoss(), optimizer=optim.Adam, learning_rate=0.001):
+
+def train_model(
+    model,
+    train_loader,
+    test_loader,
+    num_epochs,
+    device,
+    save_model=False,
+    patience=10,
+    criterion=nn.MSELoss(),
+    optimizer=optim.Adam,
+    learning_rate=0.001,
+):
     # Train the model
     total_step = len(train_loader)
-    
+
     optimizer = optimizer(model.parameters(), lr=learning_rate)
     model.to(device)
-    
-    min_loss = float('inf')
-    model_path = os.path.join(config.ROOT_DIR, 'models', f'{model}')
+
+    min_loss = float("inf")
+    model_path = os.path.join(config.ROOT_DIR, "models", f"{model}")
     best_model_state = None  # To store the state of the best model
-    
+
     if save_model:
         os.makedirs(model_path, exist_ok=True)
-    
+
     epochs_no_improve = 0
-    
+
     for epoch in tqdm(range(num_epochs)):
         model.train()
         train_loss = 0
@@ -32,9 +44,9 @@ def train_model(model, train_loader, test_loader, num_epochs, device, save_model
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-        
+
         train_loss /= total_step
-        
+
         # Validation phase
         model.eval()
         test_loss = 0
@@ -44,29 +56,31 @@ def train_model(model, train_loader, test_loader, num_epochs, device, save_model
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 test_loss += loss.item()
-        
+
         test_loss /= len(test_loader)
-        
-        print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {test_loss}')
-        
+
+        print(
+            f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Train Loss: {test_loss}"
+        )
+
         # Check if the loss improved
         if test_loss < min_loss:
             min_loss = test_loss
             best_model_state = model.state_dict()  # Save the best model state
             if save_model:
-                torch.save(best_model_state, os.path.join(model_path, 'best_model.pth'))
-            model_name = f'{model}_epoch_{epoch+1}'
+                torch.save(best_model_state, os.path.join(model_path, "best_model.pth"))
+            model_name = f"{model}_epoch_{epoch+1}"
             print(f"Improved {model_name} with Test Loss: {test_loss}")
             epochs_no_improve = 0
         else:
             epochs_no_improve += 1
-        
+
         if epochs_no_improve >= patience:
             print(f"Stopping training early - no improvement in {patience} epochs.")
             break
-    
+
     # Restore the best model before returning
     if best_model_state:
         model.load_state_dict(best_model_state)
-    
+
     return model
